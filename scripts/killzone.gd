bb8546ab96394add2966df0ac4ae9e7dc30889cb
beki-github <bereketendale9@gmail.com>
2025-12-02 18:54:55 -0300

    fixed the collision mask bug and animation scene for jump,run and idle

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,10 +1,12 @@
 extends Area2D
 @onready var timer: Timer = $Timer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you got cooked!!!")
 	Engine.time_scale=0.5
+	animated_sprite.play("death")
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
