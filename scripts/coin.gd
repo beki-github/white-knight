060b53ed5c3fe8c7a55c555ec5f1a38206af3fd3
beki-github <bereketendale9@gmail.com>
2025-12-26 19:47:34 -0300

    added sound effects for the background and for coin picking

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,6 @@
 extends Area2D
 @onready var gamemanager: Node = %Gamemanager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 func _on_body_entered(body: Node2D) -> void:
 	gamemanager.add_score()
-	queue_free()
+	animation_player.play("pickup")
--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -3,5 +3,5 @@ var score=0
 @onready var label: Label = $CanvasLayer/Label
 func add_score():
 	score+=1
-	label.text="current " + str(score)
+	label.text="current score: " + str(score)
 	print(score)
